2e8983d6969acd81d127fc401d729946645ab613
Yadav SubratKumar J. <yadavsubrat007@gmail.com>
2025-04-17 18:57:20 +0530

    Create basic_command_task.sh

---
+++ basic_command_task.sh
@@ -0,0 +1,26 @@
+          # Basic Linux Commands (Make sure to remove - before using my commands)
+          # Task: What is the linux command to
+          
+# To view what's written in a file.
+# To change the access permissions of files.
+# To check which commands you have run till now.
+# To remove a directory/ Folder.
+# To create a fruits.txt file and to view the content.
+# Add content in devops.txt (One in each line) - Apple, Mango, Banana, Cherry, Kiwi, Orange, Guava.
+# To Show only top three fruits from the file.
+# To Show only bottom three fruits from the file.
+# To create another file Colors.txt and to view the content.
+# Add content in Colors.txt (One in each line) - Red, Pink, White, Black, Blue, Orange, Purple, Grey.
+# To find the difference between fruits.txt and Colors.txt file.
+
+- cat demo.txt
+- chmod <permisson> (777,711,511 etc) demo.txt
+- history
+- rm -r demoFolder
+- nano fruits.txt 
+    cat fruits.txt
+- head -n 3 fruits.txt
+- tail -n 3 fruits.txt
+- nano colors.txt 
+    cat colors.txt
+- diff fruits.txt colors.txt
